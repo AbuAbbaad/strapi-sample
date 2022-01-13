99a7efd7faaca69f6397094a9b808eb5389f4a44
AbuAbbaad <syed@thezudo.com>
2022-01-13 07:26:16 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,4 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://https://main.d386cdzauogoix.amplifyapp.com'),
+  proxy: env.bool('IS_PROXIED', true),
 });
